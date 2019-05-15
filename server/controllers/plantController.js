737a520c6030f230812bba1dcf0252167d40de93
Tom Johnson <tomajohnson21@gmail.com>
2019-05-14 19:10:53 -0500

    Changed spacing between methods in plantController.js

--- server/controllers/plantController.js
+++ server/controllers/plantController.js
@@ -10,6 +10,7 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  
   findById: function(req, res) {
     db.Plant
       .findById(req.params.id)
